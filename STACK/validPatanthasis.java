0e481a6a89f23122c31bc1cbbcef86afb0431d4d
poorna083 <dpoorna083@gmail.com>
2025-08-31 15:22:43 +0530

    validPatanthasis

---
+++ STACK/validPatanthasis.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class validPatanthasis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        Stack<Character> c = new Stack<>();
+        for(Character i:c){
+            c.push(i);
+        }
+        so
+
+        
+
+    }
+}
