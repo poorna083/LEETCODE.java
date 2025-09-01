2f1bb30102fbf2ada2b65d11f5706ae1e106000e
poorna083 <dpoorna083@gmail.com>
2025-09-01 21:25:32 +0530

    majorityOfelement

---
+++ SORTING/majorityOfelement.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class majorityOfelement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int [] array = new int[size];
+        for(int i=0;i<array.length;i++){
+            array[i]=sc.nextInt();
+        }
+        int count = 0,candidate = 0;
+        for(int num :array){
+            if (count == 0) {
+                candidate = num;
+            }
+            count +=  (num==candidate) ? 1 : -1;
+            System.out.println(candidate);
+
+        }
+        
+    }
+}
