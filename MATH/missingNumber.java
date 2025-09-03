c73efbd99c0b217795e04f2e87e11abf43721a75
poorna083 <dpoorna083@gmail.com>
2025-09-03 22:04:40 +0530

    missingNumber

--- MATH/missingNumber.java
+++ MATH/missingNumber.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class missingNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int size = sc.nextInt();
+        int [] nums = new int[size];
     }
 }
