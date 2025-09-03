706eab541e45689fc8a9dbe46da14727aa64c6b7
poorna083 <dpoorna083@gmail.com>
2025-09-03 22:29:09 +0530

    anagram

--- STRINGS/anagram.java
+++ STRINGS/anagram.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
 public class anagram {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+    }
 }
