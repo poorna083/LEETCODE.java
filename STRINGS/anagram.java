8f437b9cab2760786847592b90c18558970eb1bc
poorna083 <dpoorna083@gmail.com>
2025-09-03 22:31:03 +0530

    anagram

--- STRINGS/anagram.java
+++ STRINGS/anagram.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
-
+class solution{
+    public boolean anagram(String s,String t){
+        
+    }
+}
 public class anagram {
     public static void main(String[] args) {
+        solution sn = new solution();
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         String t = sc.nextLine();
+        sn.anagram(s, t);
     }
 }
