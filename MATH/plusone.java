baf4c6f25305de99fe97dc3fb31e438ca4e0ca20
poorna083 <dpoorna083@gmail.com>
2025-09-04 21:50:41 +0530

    plusOne

--- MATH/plusone.java
+++ MATH/plusone.java
@@ -1,7 +1,26 @@
 import java.util.Scanner;
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+}
+
 
 public class plusone {
     Scanner sc = new Scanner(System.in);
     int size = sc.nextInt();
     int [] digits = new int[size];
+    Solution sl = new Solution();
+    sl.plusOne(digits);
+    
 }
