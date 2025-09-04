ebceb06a9eeaa9c8bafad9039ebadc064d8c06cb
poorna083 <dpoorna083@gmail.com>
2025-09-04 21:30:14 +0530

    longestPalandrome

--- GREEDY/longestPalandrome.java
+++ GREEDY/longestPalandrome.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 class Solution{
-    public int longestpalandome(String a){
-         res = a.length();
+    public int Answer(String a){
+        int res = a.length();
          return res;
     }
 }
 public class longestPalandrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
+        
+        
     }
 }
