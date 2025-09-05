55ad6c12e80caefe1c30f443e5080e0f9bf0345d
poorna083 <dpoorna083@gmail.com>
2025-09-05 20:28:31 +0530

    fibnocii

---
+++ DYNAMICPROGRAMMING/fibnocii.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Solution{
+    public int fib(int n){
+        
+    }
+}
+
+public class fibnocii {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Solution s = new Solution();
+        s.fib(n);
+    }
+}
