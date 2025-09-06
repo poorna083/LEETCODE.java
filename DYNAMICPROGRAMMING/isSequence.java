77269f3394fe9104e605c2b265fafca5403fa476
poorna083 <dpoorna083@gmail.com>
2025-09-06 18:54:05 +0530

    isSequence

--- DYNAMICPROGRAMMING/isSequence.java
+++ DYNAMICPROGRAMMING/isSequence.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+class Solution{
+    public boolean sequence(String a,String b){
+        a.
+    }
+}
 public class isSequence {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        Solution s = new Solution();
+        System.out.println(s.sequence(a, b));
+    }
 }
