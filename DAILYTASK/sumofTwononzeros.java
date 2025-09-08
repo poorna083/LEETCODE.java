59bc34219bd4a248f1b3ad789f9287681535d4e7
poorna083 <dpoorna083@gmail.com>
2025-09-08 13:15:28 +0530

    sumofTwononzeros

--- DAILYTASK/sumofTwononzeros.java
+++ DAILYTASK/sumofTwononzeros.java
@@ -4,11 +4,13 @@ public class sumofTwononzeros {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int [] array = new int[n];
+        int sum = 0;
+        int [] array = new int[2];
         for(int i=0;i<2;i++){
-            array[i]=(n%10);
-            array[i+1]=(n/10);    
-            System.out.println(array);
+            array[i]=(n/2);
+            array[i+1]=(n/2); 
+            sum += array[i]+array[i+1];   
         }
+        System.out.println(sum);
     }
 }
