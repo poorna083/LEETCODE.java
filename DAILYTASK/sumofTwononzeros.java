fb32dfd1b7a91d7ac6d25b73fbedacce1b80b8db
poorna083 <dpoorna083@gmail.com>
2025-09-08 13:19:16 +0530

    sumofTwononzeros

--- DAILYTASK/sumofTwononzeros.java
+++ DAILYTASK/sumofTwononzeros.java
@@ -6,11 +6,16 @@ public class sumofTwononzeros {
         int n = sc.nextInt();
         int sum = 0;
         int [] array = new int[2];
+        int [] arr = new int[2];
         for(int i=0;i<2;i++){
             array[i]=(n/2);
-            array[i+1]=(n/2); 
-            sum += array[i]+array[i+1];   
+            array[i+1]=n-array[i];  
         }
-        System.out.println(sum);
+        for (int i = 0; i < 2; i++) {
+            arr[i]=array[i];
+            arr[i+1]=array[i+1];
+        }
+        System.out.println(arr);
+        sc.close();
     }
 }
