3a5eba2cb346b536fa65f44a43d712a0e83b74d3
poorna083 <dpoorna083@gmail.com>
2025-09-08 13:34:10 +0530

    sumofTwononzeros

--- DAILYTASK/sumofTwononzeros.java
+++ DAILYTASK/sumofTwononzeros.java
@@ -5,15 +5,14 @@ public class sumofTwononzeros {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] array = new int[2];  
-        array[0] = n / 2;
-        array[1] = n - array[0];
-        if (array[0] == 0 || array[1] == 0) {
+        int[] array = new int[2];
+        if (n > 1) {
             array[0] = 1;
             array[1] = n - 1;
+            System.out.println(Arrays.toString(array));
+        } else {
+            System.out.println("Not possible for n=1");
         }
-        System.out.println(Arrays.toString(array));
-
         sc.close();
     }
 }
