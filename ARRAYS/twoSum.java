4b07efa56ba58c092c88b33ae339c526f2f3e885
poorna083 <dpoorna083@gmail.com>
2025-09-09 21:48:28 +0530

    twoSum

--- ARRAYS/twoSum.java
+++ ARRAYS/twoSum.java
@@ -1,15 +1,20 @@
 import java.util.*;
 class SumOfTwo{
-    public 
+    public int [] sumoftwo(int [] nums,int target){
+        
+    }
 }
 public class twoSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
+        int target = sc.nextInt();
         int [] nums = new int[size];
         for (int i = 0; i < nums.length; i++) {
             nums[i]=sc.nextInt();
         }
+        SumOfTwo st = new SumOfTwo();
+        st.
         sc.close();
     }
 }
