7202f373694a129b2149aa7d15cf9eda64c532c7
poorna083 <dpoorna083@gmail.com>
2025-09-09 19:08:43 +0530

    rotateString

--- STRINGS/rotateString.java
+++ STRINGS/rotateString.java
@@ -4,14 +4,14 @@ class rotatestr{
         if(s.length()!=goal.length())return false;
         else if(s.length()==goal.length()){
             for (int i = 0; i < s.length(); i++) {
-                for (int j = 0; j < goal.length(); j++) {
-                    if (s.charAt(i)==goal.charAt(i)) {
-                        
+                for (int j = i+1; j < goal.length(); j++) {
+                    if (s.charAt(i)==goal.charAt(j)) {
+                        return true;
                     }
                 }
             }
             
-        }
+        }return true;
     }
 }
 public class rotateString {
