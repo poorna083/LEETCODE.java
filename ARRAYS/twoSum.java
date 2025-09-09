075e04cef6046f79934ced779de73c19358579da
poorna083 <dpoorna083@gmail.com>
2025-09-09 21:51:05 +0530

    twoSum

--- ARRAYS/twoSum.java
+++ ARRAYS/twoSum.java
@@ -3,9 +3,11 @@ class SumOfTwo{
     public int [] sumoftwo(int [] nums,int target){
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
-                
+                if (nums[i]+nums[j]==target) {
+                    return new int[] {i,j};
+                }
             }
-        }
+        }return new int[]{};
     }
 }
 public class twoSum {
