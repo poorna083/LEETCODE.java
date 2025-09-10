3eef9e20d75ee6d2ec3e65afc4fd057e6e4ec9a1
poorna083 <dpoorna083@gmail.com>
2025-09-10 17:29:07 +0530

    isomatricString

--- HASHTABLE/isomatricString.java
+++ HASHTABLE/isomatricString.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
+ class InnerisomatricString {
+    public boolean innerisometric(String s,String t){
+
+    }re
+    
+}
 
 public class isomatricString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+        InnerisomatricString i = new InnerisomatricString();
+        i.innerisometric(s, t);
+        sc.close();
     }
 }
