5b3f0d4be4c83e375ca4ed6bd1caf18d7e47421f
poorna083 <dpoorna083@gmail.com>
2025-09-10 17:54:21 +0530

    containsDuplicate

---
+++ ARRAYS/containsDuplicate.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ class InnercontainsDuplicate {
+    public boolean containsduplicate(int k , int[]nums){
+
+    }
+    
+}
+
+public class containsDuplicate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        System.out.println("Size");
+        int [] nums = new int[size];
+        System.out.println("Array elements:");
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=sc.nextInt();
+        }
+        int k = sc.nextInt();
+        
+    }
+}
