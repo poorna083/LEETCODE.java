da3e08fe0bd04c0aa22de47a6b035a205edb8c88
poorna083 <dpoorna083@gmail.com>
2025-09-11 18:08:29 +0530

    ovealSorting

--- DAILYTASK/sortOvealinstring.java
+++ DAILYTASK/sortOvealinstring.java
@@ -1,7 +1,25 @@
 import java.util.Scanner;
 class ovealsort{
-    public String ovealSorting(String s){
+    public String ovealSorting(String s) {
+        fint n = s.length();
+        ArrayList<Character> vow = new ArrayList<>();
+        ArrayList<Integer> pos = new ArrayList<>();
         
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
+ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') { 
+                vow.add(ch);
+                pos.add(i); 
+            }
+        }  
+        Collections.sort(vow);
+        char[] answer = s.toCharArray();
+        for (int i = 0; i < pos.size(); i++) {
+            answer[pos.get(i)] = vow.get(i);
+        }
+        
+        return new String (answer);
     }
 }
 public class sortOvealinstring {
