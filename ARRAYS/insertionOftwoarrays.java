67eb6ca6d968d74f913a948a04cf8959e1940313
poorna083 <dpoorna083@gmail.com>
2025-09-12 21:32:22 +0530

    insertionOftwoarrays

--- ARRAYS/insertionOftwoarrays.java
+++ ARRAYS/insertionOftwoarrays.java
@@ -1,39 +1,54 @@
-import java.util.Arrays;
-import java.util.Scanner;
-class insertionoftwo{
-    public int[] insertionoftwoArray(int [] nums1,int [] nums2){
-        int[] duplicate = new int[nums1.length+nums2.length];
-        for (int i = 0; i < duplicate.length; i++) {
-            duplicate[i]= nums1[i]+nums2[i];
+import java.util.*;
+
+class insertionoftwo {
+    public int[] insertionoftwoArray(int[] nums1, int[] nums2) {
+        // Use HashSet to store unique elements
+        Set<Integer> set1 = new HashSet<>();
+        for (int n : nums1) {
+            set1.add(n);
         }
-        Arrays.sort(duplicate);
-        for (int i = 0; i < duplicate.length; i++) {
-            for (int j = i+1; j < duplicate.length; j++) {
-                if (duplicate[i]==duplicate[j]) {
-                    
-                }
+
+        Set<Integer> result = new HashSet<>();
+        for (int n : nums2) {
+            if (set1.contains(n)) {
+                result.add(n); // intersection
             }
         }
-        return duplicate;
+
+        // Convert Set -> Array
+        int[] intersection = new int[result.size()];
+        int i = 0;
+        for (int n : result) {
+            intersection[i++] = n;
+        }
+
+        return intersection;
     }
 }
 
 public class insertionOftwoarrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Size of 1");
-        int size = sc.nextInt();
-        int [] nums1 = new int[size];
+        int size1 = sc.nextInt();
+        int[] nums1 = new int[size1];
+        System.out.println("Enter elements of first array:");
+        for (int i = 0; i < size1; i++) {
+            nums1[i] = sc.nextInt();
+        }
+
         System.out.println("Size of 2");
-        int siz = sc.nextInt();
-        int [] nums2 = new int[siz];
-        for (int i = 0; i < nums1.length; i++) {
-            nums1[i]=sc.nextInt();
+        int size2 = sc.nextInt();
+        int[] nums2 = new int[size2];
+        System.out.println("Enter elements of second array:");
+        for (int i = 0; i < size2; i++) {
+            nums2[i] = sc.nextInt();
         }
-        for (int i = 0; i < nums2.length; i++) {
-            nums2[i]=sc.nextInt();
-        }
+
         insertionoftwo it = new insertionoftwo();
-        System.out.println(it.insertionoftwoArray(nums1, nums2));
+        int[] result = it.insertionoftwoArray(nums1, nums2);
+
+        System.out.println("Intersection: " + Arrays.toString(result));
     }
 }
