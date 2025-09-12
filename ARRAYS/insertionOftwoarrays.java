6b5a909e7177f36547b733fea4601440647fa47a
poorna083 <dpoorna083@gmail.com>
2025-09-12 21:13:30 +0530

    insertionOftwoarrays

---
+++ ARRAYS/insertionOftwoarrays.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class insertionOftwoarrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Size of 1");
+        int size = sc.nextInt();
+        int [] nums1 = new int[size];
+    }
+}
