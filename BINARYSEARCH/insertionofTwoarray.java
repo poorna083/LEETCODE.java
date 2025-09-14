296ab770647abb3c9b1d9cb42ed1aa00b6760622
poorna083 <dpoorna083@gmail.com>
2025-09-14 19:25:13 +0530

    insertionofTwoarray

--- BINARYSEARCH/insertionofTwoarray.java
+++ BINARYSEARCH/insertionofTwoarray.java
@@ -1,3 +1,23 @@
-public class insertionofTwoarray {
+import java.util.Scanner;
+public class InnerinsertionofTwoarray {
+    public int[] intersection(int[] array1, int[] array2) {
+        
+    }
     
+}
+
+public class insertionofTwoarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size1 = sc.nextInt();
+        int size2 = sc.nextInt();
+        int [] array1 = new int[size1];
+        int [] array2 = new int[size2];
+        for (int i = 0; i < array1.length; i++) {
+            array1[i]=sc.nextInt();
+        } 
+        for (int i = 0; i < array1.length; i++) {
+            array2[i]=sc.nextInt();
+        }
+    }
 }
