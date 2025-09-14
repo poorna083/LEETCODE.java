485f8cfab671c48ff12ac665ffcfab77b54131da
poorna083 <dpoorna083@gmail.com>
2025-09-14 20:38:48 +0530

    insertionofTwoarray

--- BINARYSEARCH/insertionofTwoarray.java
+++ BINARYSEARCH/insertionofTwoarray.java
@@ -23,20 +23,25 @@ class InnerinsertionofTwoarray {
 public class insertionofTwoarray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < array2.length; i++) {
-            array2[i]=sc.nextInt();
+        System.out.print("Enter size of array1: ");
+        int n1 = sc.nextInt();
+        int[] array1 = new int[n1];
+        System.out.println("Enter elements of array1:");
+        for (int i = 0; i < n1; i++) {
+            array1[i] = sc.nextInt();
         }
+        System.out.print("Enter size of array2: ");
+        int n2 = sc.nextInt();
+        int[] array2 = new int[n2];
+        System.out.println("Enter elements of array2:");
+        for (int i = 0; i < n2; i++) {
+            array2[i] = sc.nextInt();
+        }
+        
         InnerinsertionofTwoarray i = new InnerinsertionofTwoarray();
         int[] result = i.intersection(array1, array2);
         for (int val : result) {
             System.out.print(val + " ");
         }
-    }
-}
-        for (int i = 0; i < array1.length; i++) {
-            array2[i]=sc.nextInt();
-        }
-        InnerinsertionofTwoarray i = new InnerinsertionofTwoarray();
-        System.out.println(i.intersection(array1, array2));
     }
 }
