b467894644a3e470101b6ac5a8f3264a119f4f82
poorna083 <dpoorna083@gmail.com>
2025-09-15 17:56:22 +0530

    maxWordtype

--- STRINGS/maxWordtype.java
+++ STRINGS/maxWordtype.java
@@ -1,3 +1,13 @@
-public class maxWordtype {
+import java.util.Scanner;
+ class InnermaxWordtype {
+    public int maxwordtyppe(String s,String broke);
     
+}
+
+public class maxWordtype {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String broke = sc.nextLine();
+    }
 }
