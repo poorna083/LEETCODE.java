77bf6057e58276beb76f3da7dde0c98b0fc60bd5
poorna083 <dpoorna083@gmail.com>
2025-09-16 23:03:41 +0530

    noncoPrime

--- DAILYTASK/noncoPrime.java
+++ DAILYTASK/noncoPrime.java
@@ -5,5 +5,9 @@ public class noncoPrime {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size");
         int size = sc.nextInt();
+        int [] A = new int[size];
+        for (int i = 0; i < A.length; i++) {
+            A[i]=sc.nextInt();
+        }
     }
 }
