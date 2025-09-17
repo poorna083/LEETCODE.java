cbb39ec56afa4e96dffd719f4e5267a067d36e8f
poorna083 <dpoorna083@gmail.com>
2025-09-17 21:59:53 +0530

    anagram1

--- STRINGS/anagram1.java
+++ STRINGS/anagram1.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
+import java.util.*;
 class Inneranagram1 {
-    public boolean isAnagram(String s ,String t);
-    
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        char[] sSort = s.toCharArray();
+        char[] tSort = t.toCharArray();
+        Arrays.sort(sSort);
+        Arrays.sort(tSort);
+        return Arrays.equals(sSort, tSort);
+    }
 }
-
 public class anagram1 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+       public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
         String s = sc.nextLine();
         String t = sc.nextLine();
-        Inneranagram1 i1 = new Inneranagram1();
-        i1.isAnagram(s, t);
-
-    }
+        
+       }
 }
