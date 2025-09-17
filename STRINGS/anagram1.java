547dec12dfeba8423ed7129ad35624d21a56ec0f
poorna083 <dpoorna083@gmail.com>
2025-09-17 21:46:44 +0530

    anagram1

---
+++ STRINGS/anagram1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Inneranagram1 {
+    public boolean 
+    
+}
+
+public class anagram1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+
+    }
+}
