2aecb1fba3134f63e73de0056f057e9f5ccc39eb
poorna083 <dpoorna083@gmail.com>
2025-09-17 06:29:38 +0530

    reversePrefix

--- STACK/reversePrefix.java
+++ STACK/reversePrefix.java
@@ -1,3 +1,13 @@
-public class reversePrefix {
+import java.util.Scanner;
+ class InnerreversePrefix {
+
     
+}
+
+public class reversePrefix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        char c = sc.next().charAt(0);
+    }
 }
