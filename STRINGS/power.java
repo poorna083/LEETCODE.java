d6970f8cd1d877729690f09d20193ed106134ff1
poorna083 <dpoorna083@gmail.com>
2025-09-18 17:58:50 +0530

    pow(x,n)

--- STRINGS/power.java
+++ STRINGS/power.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
+class Innerpower {
 
+    
+}
 public class power {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        double d = sc.nextDouble();
+        int n = sc.nextInt();
+
     }
 }
