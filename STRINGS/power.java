1711f9803d39fd7ce70374632f5f913b18a19466
poorna083 <dpoorna083@gmail.com>
2025-09-18 18:06:53 +0530

    myPow

--- STRINGS/power.java
+++ STRINGS/power.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
+
 class Innerpower {
     public double myPow(double d, int n) {
         if (n == 0) {
             return 1;
         }
-        if (n >=0) {
-            return 1 / myPow(d, -n);
+        if (n < 0) {
+            return 1 / myPow(d, -n); 
         }
         return d * myPow(d, n - 1);
     }
-    
 }
+
 public class power {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -19,6 +20,5 @@ public class power {
         Innerpower i = new Innerpower();
         System.out.println(i.myPow(d, n));
         sc.close();
-
     }
 }
