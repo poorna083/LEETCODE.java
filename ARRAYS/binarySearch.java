d620e86bdf3d52a434fdd57cd6299e63527052f6
poorna083 <dpoorna083@gmail.com>
2025-09-19 18:11:48 +0530

    binarySearch

--- ARRAYS/binarySearch.java
+++ ARRAYS/binarySearch.java
@@ -1,3 +1,21 @@
-public class binarySearch {
+import java.util.Scanner;
+class InnerbinarySearch {
+    public int search(int[] nums, int target) {
+        
+    }
     
+}
+public class binarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size:");
+        int size = sc.nextInt();
+        System.out.println("Enter the arrays");
+        int [] nums = new int[size];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=sc.nextInt();
+        }
+        System.out.println("Enter The Target");
+        int target = sc.nextInt();
+    }
 }
