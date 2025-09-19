54844772422520221e0383ddf840a34295483603
poorna083 <dpoorna083@gmail.com>
2025-09-19 18:25:09 +0530

    findDifference

--- SORTING/findDifference.java
+++ SORTING/findDifference.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+class InnerfindDifference {
+
+    
+}
 public class findDifference {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
     }
 }
