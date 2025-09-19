e981f44cebb62a231cd218b5cceaa236c176f4a7
poorna083 <dpoorna083@gmail.com>
2025-09-19 18:30:38 +0530

    findDifference

--- SORTING/findDifference.java
+++ SORTING/findDifference.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 class InnerfindDifference {
     public char findTheDifference(String s, String t) {
-        
+        if(s.isEmpty() || t.isEmpty())return 0;
+        char a [] = s.toCharArray();
+        char b [] = t.toCharArray();
     }
     
 }
