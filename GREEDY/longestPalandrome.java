a033714ec324b385c8f932b07dab5287a8a8ba58
poorna083 <dpoorna083@gmail.com>
2025-09-20 10:15:27 +0530

    removeDuplicate

--- GREEDY/longestPalandrome.java
+++ GREEDY/longestPalandrome.java
@@ -1,3 +1,29 @@
-public class longestPalandrome {
+import java.util.Scanner;
+class InnerlongestPalandrome {
+   public int longestPalindrome(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int[] count = new int[128];
+        for (char c : s.toCharArray()) {
+            count[c]++;
+        }
+        int result = 0;
+        for (int v : count) {
+            result += v / 2 * 2;
+            if (result % 2 == 0 && v % 2 == 1) {
+                result++;
+            }
+        }
+        return result;
+    }
     
+}
+
+public class longestPalandrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        InnerlongestPalandrome l = new InnerlongestPalandrome();
+        System.out.println(l.longestPalindrome(s));
+        sc.close();
+    }
 }
