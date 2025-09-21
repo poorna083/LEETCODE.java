820337b7fd4c9a1563dbc4b5e1512a7a9dffd304
poorna083 <dpoorna083@gmail.com>
2025-09-21 18:25:48 +0530

    hexadecimal

--- MATH/hexadecimal.java
+++ MATH/hexadecimal.java
@@ -1,3 +1,12 @@
-public class hexadecimal {
+import java.util.Scanner;
+class Innerhexadecimal {
+
     
+}
+
+public class hexadecimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+    }
 }
