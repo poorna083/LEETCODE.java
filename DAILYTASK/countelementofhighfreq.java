f8dac0a8429df7deb18899888c801a130ab9bf0c
poorna083 <dpoorna083@gmail.com>
2025-09-22 18:29:59 +0530

    countelementofhighfreq

--- DAILYTASK/countelementofhighfreq.java
+++ DAILYTASK/countelementofhighfreq.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-
+class Innercountelementofhighfreq {
+    public int maxFrequencyElements(int[] array) {
+        
+    }
+    
+}
 public class countelementofhighfreq {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,5 +15,7 @@ public class countelementofhighfreq {
         for (int i = 0; i < array.length; i++) {
             array[i]=sc.nextInt();
         }
+        Innercountelementofhighfreq i = new Innercountelementofhighfreq();
+        System.out.println(i.maxFrequencyElements(array));
     }
 }
