bdd551e6c00b94f4813ab6838f547413efca4da6
poorna083 <dpoorna083@gmail.com>
2025-09-22 18:25:39 +0530

    countelementofhighfreq

--- DAILYTASK/countelementofhighfreq.java
+++ DAILYTASK/countelementofhighfreq.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class countelementofhighfreq {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size:");
         
     }
 }
