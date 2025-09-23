af87ed938200298c60aacdd38f0545b9db432cb9
poorna083 <dpoorna083@gmail.com>
2025-09-23 18:11:09 +0530

    versions

--- DAILYTASK/versions.java
+++ DAILYTASK/versions.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
 public class versions {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String version1 = sc.nextLine();
+        String version2 = sc.nextLine();
+    }
 }
