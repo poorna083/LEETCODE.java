3194c981767a15886e23b8fec1507dca57220d95
poorna083 <dpoorna083@gmail.com>
2025-09-23 18:32:00 +0530

    versions

--- DAILYTASK/versions.java
+++ DAILYTASK/versions.java
@@ -1,8 +1,19 @@
 import java.util.Scanner;
 class InnerVersion{
     public int compareVersion(String version1, String version2) {
-        if (version1.isEmpty()||version2.isEmpty())return 0;\
-        
+        if (version1.isEmpty()||version2.isEmpty()) return 0;
+        StringBuffer sb = new StringBuffer(version1);
+        Character c1 = '.';
+        boolean contains = sb.indexOf(String.valueOf(c1))!= -1;
+        if (contains) {
+            
+        }
+        StringBuffer sb1 = new StringBuffer(version2);
+        Character c2 = '.';
+        boolean contain = sb.indexOf(String.valueOf(c2))!= -1;
+        if(contain){
+
+        }
     }
 }
 public class versions {
@@ -11,7 +22,7 @@ public class versions {
         String version1 = sc.nextLine();
         String version2 = sc.nextLine();
         InnerVersion i = new InnerVersion();
-        i.compareVersion(version1, version2);
+        System.out.println(i.compareVersion(version1, version2));
         sc.close();
     }
 }
