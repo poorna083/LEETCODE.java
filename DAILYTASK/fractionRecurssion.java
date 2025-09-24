ba0d459cb0a3a085779257fa51173278f40d21ba
poorna083 <dpoorna083@gmail.com>
2025-09-24 18:47:49 +0530

    fractionRecurssion

--- DAILYTASK/fractionRecurssion.java
+++ DAILYTASK/fractionRecurssion.java
@@ -1,9 +1,17 @@
 import java.util.*;
+class InnerfractionRecurssion {
+public String fractionToDecimal(int numerator, int denominator){
+    double result = (double) numerator / denominator;
+        return String.valueOf(result);
+}
+    
+}
 public class fractionRecurssion {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int numerator = sc.nextInt();
         int denominator = sc.nextInt();
-
+        InnerfractionRecurssion i = new InnerfractionRecurssion();
+        System.out.println(i.fractionToDecimal(numerator, denominator));
     }
 }
