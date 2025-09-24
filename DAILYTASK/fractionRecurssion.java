ccaaf28ecd6102d888f60ec7720fa6b43e22475b
poorna083 <dpoorna083@gmail.com>
2025-09-24 19:06:38 +0530

    fractionRecurssion

--- DAILYTASK/fractionRecurssion.java
+++ DAILYTASK/fractionRecurssion.java
@@ -1,8 +1,30 @@
 import java.util.*;
 class InnerfractionRecurssion {
 public String fractionToDecimal(int numerator, int denominator){
-    double result = (double) numerator / denominator;
-        return String.valueOf(result);
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) return "0";
+        StringBuilder res = new StringBuilder();
+        if ((numerator < 0) ^ (denominator < 0)) res.append("-");
+        long num = Math.abs((long) numerator);
+        long den = Math.abs((long) denominator);
+        res.append(num / den);
+        long remainder = num % den;
+        if (remainder == 0) return res.toString();
+        res.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        while (remainder != 0) {
+            if (map.containsKey(remainder)) {
+                res.insert(map.get(remainder), "(");
+                res.append(")");
+                break;
+            }
+            map.put(remainder, res.length());
+            remainder *= 10;
+            res.append(remainder / den);
+            remainder %= den;
+        }
+        return res.toString();
+    }   return String.valueOf(result);
 }
     
 }
