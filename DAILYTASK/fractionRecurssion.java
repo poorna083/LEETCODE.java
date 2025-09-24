7899ca1a752d9cc9c146c4da5547a485ddbd39ec
poorna083 <dpoorna083@gmail.com>
2025-09-24 18:31:50 +0530

    fractionRecurssion

---
+++ DAILYTASK/fractionRecurssion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class fractionRecurssion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numerator = sc.nextInt();
+        int denominator = sc.nextInt();
+
+    }
+}
