d2b4899a157b616ed80b798a1f5131b9c377bdb2
poorna083 <dpoorna083@gmail.com>
2025-09-25 17:58:02 +0530

    divisorGame

---
+++ DAILYTASK/divisorGame.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class InnerdivisorGame{
+    public boolean Divisor(int n){
+        
+    }
+}
+
+public class divisorGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        InnerdivisorGame i = new InnerdivisorGame();
+        i.Divisor(n);
+    }
+}
