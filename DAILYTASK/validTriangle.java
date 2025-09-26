0768b4d2a03bf9779bd21f72c6d0b7f4afe96a4a
poorna083 <dpoorna083@gmail.com>
2025-09-26 22:23:50 +0530

    validTriangle

--- DAILYTASK/validTriangle.java
+++ DAILYTASK/validTriangle.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class validTriangle {
     public static void main(String[] args) {
-        
+       Scanner sc = new Scanner(System.in);
+       int size = sc.nextInt();
+       int [] array = new int[size];
+       for (int i = 0; i < size; i++) {
+        array[i]=sc.nextInt();
+       } 
+       
     }
 }
