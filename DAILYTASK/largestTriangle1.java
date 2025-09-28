77384d83d6f26190296427ccf8ee78b29eb52b05
poorna083 <dpoorna083@gmail.com>
2025-09-28 18:12:30 +0530

    largestTriangle1

--- DAILYTASK/largestTriangle1.java
+++ DAILYTASK/largestTriangle1.java
@@ -3,5 +3,10 @@ import java.util.Scanner;
 public class largestTriangle1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int [] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            
+        }
     }
 }
