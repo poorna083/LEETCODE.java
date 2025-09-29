18e399f1d4c4dd2095af136f2c4312b8287ce358
poorna083 <dpoorna083@gmail.com>
2025-09-29 18:21:33 +0530

    trinagulationOfpolygin

---
+++ DAILYTASK/trinagulationOfpolygin.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class trinagulationOfpolygin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The size:");
+        int size = sc.nextInt();
+    }
+}
