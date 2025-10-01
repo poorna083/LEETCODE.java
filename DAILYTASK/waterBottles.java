411a3b0c54831437e06a5af5b56d55500446e6be
poorna083 <dpoorna083@gmail.com>
2025-10-01 18:10:11 +0530

    waterBottles

--- DAILYTASK/waterBottles.java
+++ DAILYTASK/waterBottles.java
@@ -1,3 +1,14 @@
-public class waterBottles {
+import java.util.Scanner;
+class InnerwaterBottles {
+
     
+}
+
+public class waterBottles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numbottle = sc.nextInt();
+        int numExchange = sc.nextInt();
+
+    }
 }
