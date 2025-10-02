c45d292c80b2c70afab1e117181022428785dc1d
poorna083 <dpoorna083@gmail.com>
2025-10-02 07:02:45 +0530

    WaterBottle2

---
+++ DAILYTASK/WaterBottle2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class InnerWaterBottle2 {
+    
+    
+    
+    
+    
+}
+public class WaterBottle2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numBottle = sc.nextInt();
+        int numExchange = sc.nextInt();
+        InnerWaterBottle2 i = new InnerWaterBottle2();
+    }
+}
