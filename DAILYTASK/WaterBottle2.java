3fb15e5f1fff9fbb6bb63cb0a83797959f8e2324
poorna083 <dpoorna083@gmail.com>
2025-10-02 07:03:54 +0530

    WaterBottle2

--- DAILYTASK/WaterBottle2.java
+++ DAILYTASK/WaterBottle2.java
@@ -1,10 +1,16 @@
 import java.util.*;
 class InnerWaterBottle2 {
-    
-    
-    
-    
-    
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int sum=numBottles,bottle=numBottles;
+        while(bottle >= numExchange)
+        {
+            bottle-=numExchange;
+            numExchange++;
+            sum++;
+            bottle++;
+        }
+        return sum;
+    }  
 }
 public class WaterBottle2 {
     public static void main(String[] args) {
@@ -12,5 +18,6 @@ public class WaterBottle2 {
         int numBottle = sc.nextInt();
         int numExchange = sc.nextInt();
         InnerWaterBottle2 i = new InnerWaterBottle2();
+        
     }
 }
