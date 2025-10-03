40c46f2564db17637af4f3e2006c0ed5d090831f
poorna083 <dpoorna083@gmail.com>
2025-10-03 17:21:47 +0530

    excelSheet

--- MATH/excelSheet.java
+++ MATH/excelSheet.java
@@ -1,8 +1,22 @@
 import java.util.Scanner;
+class InnerexcelSheet {
+    public String convertToTitle(int n) {
+        StringBuilder result = new StringBuilder();
 
+        while(n>0){
+            n--;
+            result.insert(0, (char)('A' + n % 26));
+            n /= 26;
+        }
+
+        return result.toString();
+    }
+    
+}
 public class excelSheet {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
     }
 }
