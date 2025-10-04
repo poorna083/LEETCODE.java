bb4f6451f6313f8f46a3b902e2af4e0a9e876021
poorna083 <dpoorna083@gmail.com>
2025-10-04 18:57:28 +0530

    excelcolumn

--- MATH/excelcolumn.java
+++ MATH/excelcolumn.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
-
+class Innerexcelcolumn {
+    public int titleToNumber(String s) {
+        if (s == null) return -1;
+        int sum = 0;
+        for (char c : s.toUpperCase().toCharArray()) {
+            sum *= 26;
+            sum += c - 'A' + 1;
+        }
+        return sum;
+    }
+}
 public class excelcolumn {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        
+
         sc.close();
     }
 }
