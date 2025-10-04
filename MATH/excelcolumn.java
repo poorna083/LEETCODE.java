403e875fdc75a1406d12e300f26c7fb97658cfca
poorna083 <dpoorna083@gmail.com>
2025-10-04 18:56:12 +0530

    excelcolumn

---
+++ MATH/excelcolumn.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class excelcolumn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        
+        sc.close();
+    }
+}
