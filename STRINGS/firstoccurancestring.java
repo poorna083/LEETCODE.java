28962b6930baa7b9574cbada25f15d79a71d2fba
poorna083 <dpoorna083@gmail.com>
2025-10-05 13:43:57 +0530

    firstoccurancestring

--- STRINGS/firstoccurancestring.java
+++ STRINGS/firstoccurancestring.java
@@ -1,4 +1,15 @@
 import java.util.Scanner;
+class Innerfirstoccurancestring {
+    public int strStr(String haystack, String needle) {
+  for (int i = 0; ; i++) {
+    for (int j = 0; ; j++) {
+      if (j == needle.length()) return i;
+      if (i + j == haystack.length()) return -1;
+      if (needle.charAt(j) != haystack.charAt(i + j)) break;
+    }
+  }
+}
+}
 
 public class firstoccurancestring {
     public static void main(String[] args) {
