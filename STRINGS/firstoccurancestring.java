f12ff1a04b74907517ad8b497169620d78b4b975
poorna083 <dpoorna083@gmail.com>
2025-10-05 13:42:42 +0530

    firstoccurancestring

--- STRINGS/firstoccurancestring.java
+++ STRINGS/firstoccurancestring.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class firstoccurancestring {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String heystack = sc.nextLine();
+        String needle = sc.nextLine();
     }
 }
