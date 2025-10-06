ac96150dd2653b656541c3a59ad8d3c8e413f194
poorna083 <dpoorna083@gmail.com>
2025-10-06 20:48:38 +0530

    thirdMaximumNumber

--- ARRAYS/thirdMaximumNumber.java
+++ ARRAYS/thirdMaximumNumber.java
@@ -1,3 +1,20 @@
-public class thirdMaximumNumber {
+import java.util.Scanner;
+class InnerthirdMaximumNumber {
+public int thirdmax(int [] nums){
+
+}
     
+}
+public class thirdMaximumNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int [] nums = new int[size];
+        for(int i=0;i<size;i++){
+            nums[i]=sc.nextInt();
+        }
+        sc.close();
+        InnerthirdMaximumNumber i = new InnerthirdMaximumNumber();
+        System.out.println(i.thirdmax(nums));
+    }
 }
