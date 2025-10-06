88a05ccb40232b63a3e7631bdaaa311541883c83
poorna083 <dpoorna083@gmail.com>
2025-10-06 21:09:47 +0530

    thirdMaximumNumber

--- ARRAYS/thirdMaximumNumber.java
+++ ARRAYS/thirdMaximumNumber.java
@@ -5,12 +5,25 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 class InnerthirdMaximumNumber {
-public int thirdmax(int [] nums){
-   Integer[] numsObj = Arrays.stream(nums).boxed().toArray(Integer[]::new);
-   Arrays.sort(numsObj, Collections.reverseOrder());
-   
-
-}
+    public int thirdMax(int[] nums) {
+        Integer max1 = null;
+        Integer max2 = null;
+        Integer max3 = null;
+        for (Integer n : nums) {
+            if (n.equals(max1) || n.equals(max2) || n.equals(max3)) continue;
+            if (max1 == null || n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (max2 == null || n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (max3 == null || n > max3) {
+                max3 = n;
+            }
+        }
+        return max3 == null ? max1 : max3;
+    }
     
 }
 public class thirdMaximumNumber {
@@ -23,6 +36,6 @@ public class thirdMaximumNumber {
         }
         sc.close();
         InnerthirdMaximumNumber i = new InnerthirdMaximumNumber();
-        System.out.println(i.thirdmax(nums));
+        System.out.println(i.thirdMax(nums));
     }
 }
