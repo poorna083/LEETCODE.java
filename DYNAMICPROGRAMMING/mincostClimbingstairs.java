0e7cd47b65f156269845d39948a195b0c5fd48ae
poorna083 <dpoorna083@gmail.com>
2025-10-07 18:21:13 +0530

    mincostClimbingstairs

--- DYNAMICPROGRAMMING/mincostClimbingstairs.java
+++ DYNAMICPROGRAMMING/mincostClimbingstairs.java
@@ -1,7 +1,11 @@
 import java.util.*;
 class InnermincostClimbingstairs {
     public int mincostclimbingStairs(int [] cost){
-        
+        int n = cost.length ;
+        for(int i=2 ; i<n ; i++){
+            cost[i] = Math.min(cost[i-1] , cost[i-2]) + cost[i];
+        }
+        return Math.min(cost[n-1] , cost[n-2]);
     }
     
 }
