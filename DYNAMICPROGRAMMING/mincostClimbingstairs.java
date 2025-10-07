b3d9c042599fb747141ac42fd470d7f53419c3ab
poorna083 <dpoorna083@gmail.com>
2025-10-07 18:04:33 +0530

    mincostClimbingstairs

--- DYNAMICPROGRAMMING/mincostClimbingstairs.java
+++ DYNAMICPROGRAMMING/mincostClimbingstairs.java
@@ -1,3 +1,18 @@
-public class mincostClimbingstairs {
+import java.util.*;
+class InnermincostClimbingstairs {
+
     
+}
+public class mincostClimbingstairs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The size:");
+        int size = sc.nextInt();
+        System.out.println("Enter The Elements:");
+        int [] cost = new int[size];
+        for(int i=0;i<size;i++){
+            cost[i]=sc.nextInt();
+        }
+        sc.close();
+    }
 }
