05576e9b935a01bfb528c47ff6d536c3adfb5b46
poorna083 <dpoorna083@gmail.com>
2025-10-08 18:18:03 +0530

    moveZeros

--- ARRAYS/moveZeros.java
+++ ARRAYS/moveZeros.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class moveZeros {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size:");
+        int size = sc.nextInt();
+        System.out.println("Enter The array Elements");
+        int [] nums = new int[size];
+        for(int i=0;i<size;i++){
+            nums[i]= sc.nextInt();
+        }
         
     }
 }
