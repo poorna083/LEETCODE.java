fabbea4f74fe7efdc82da21b84ed68c30eecda52
poorna083 <dpoorna083@gmail.com>
2025-10-12 10:39:58 +0530

    maxreportingSubstring

--- DYNAMICPROGRAMMING/maxreportingSubstring.java
+++ DYNAMICPROGRAMMING/maxreportingSubstring.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+class MaxreportingSubstring{
+    public int maxRepeting(String Sequence, String word){
+        
+            char[] seq = Sequence.toCharArray();
+        
+    }
+}
 public class maxreportingSubstring {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String Sequence = sc.nextLine();
+        String word = sc.nextLine();
+        MaxreportingSubstring i = new MaxreportingSubstring();
+        System.out.println(i.maxRepeting(Sequence, word));
+        sc.close();
+    }
 }
