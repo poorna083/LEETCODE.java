3cd5d11de9e536e0960a978094ed95938289b990
poorna083 <dpoorna083@gmail.com>
2025-10-13 18:20:44 +0530

    countingBytes

---
+++ DYNAMICPROGRAMMING/countingBytes.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class InnercountingBytes{
+    public int [] counting(int [] array){
+        int[] f = new int[array + 1];
+    for (int i=1; i<=array.length; i++) f[i] = f[i >> 1] + (i & 1);
+    return f;
+    }
+}
+public class countingBytes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Size:");
+        int size = sc.nextInt();
+        System.out.println("Enter The array:");
+        int [] array = new int[size];
+        for(int i=0;i<size;i++){
+            array[i]=sc.nextInt();
+        }
+        sc.close();
+    }
+}
