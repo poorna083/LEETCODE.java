ede3283f8fa9f2a1bdff5775efe4c7e05e259019
poorna083 <dpoorna083@gmail.com>
2025-10-14 18:09:37 +0530

    InnerincreasingSubarrays

--- DAILYTASK/increasingSubarrays.java
+++ DAILYTASK/increasingSubarrays.java
@@ -1,4 +1,9 @@
 import java.util.*;
+class InnerincreasingSubarrays{
+    public boolean hasIncreasingSubarrays(List<Integer> list ,int k){
+
+    }
+}
 public class increasingSubarrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,7 +13,11 @@ public class increasingSubarrays {
         for(int i=0;i<size;i++){
             list.add(sc.nextInt());
         }
+        int k = sc.nextInt();
         sc.close();
+        InnerincreasingSubarrays i = new InnerincreasingSubarrays();
+        System.out.println(i.hasIncreasingSubarrays(list, k));
+
 
     }
 }
