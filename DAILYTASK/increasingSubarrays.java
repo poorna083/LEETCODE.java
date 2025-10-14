4bd4c0c3d70b3c27adff2dc630dc71e5ef96bc7a
poorna083 <dpoorna083@gmail.com>
2025-10-14 18:21:46 +0530

    increasingSubarrays

--- DAILYTASK/increasingSubarrays.java
+++ DAILYTASK/increasingSubarrays.java
@@ -2,7 +2,25 @@ import java.util.*;
 class InnerincreasingSubarrays{
     public boolean hasIncreasingSubarrays(List<Integer> list ,int k){
         int size = list.size();
+        List <Integer> firsthalf = new ArrayList<>();
+        List <Integer> Secondhalf = new ArrayList<>();
+        for(int i=0;i<size/2;i++){
+            firsthalf.add(list.get(i));
+        }
+        for(int i=size/2;i<size;i++){
+            Secondhalf.add(list.get(i));
+        }
+        for(int i=0;i<k;i++){
+            if(firsthalf.get(i) == k) return true;
+            else return false;
+        }
+        for(int i=0;i<k;i++){
+            if(Secondhalf.get(i) == k) return true;
+            else return false;
+        }
         
+
+
     }
 }
 public class increasingSubarrays {
