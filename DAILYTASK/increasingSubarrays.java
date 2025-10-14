448c02201072440e13a9ef038b76e2080931f036
poorna083 <dpoorna083@gmail.com>
2025-10-14 18:06:51 +0530

    increasingSubarrays

--- DAILYTASK/increasingSubarrays.java
+++ DAILYTASK/increasingSubarrays.java
@@ -1,7 +1,14 @@
 import java.util.*;
 public class increasingSubarrays {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         List<Integer> list = new ArrayList<>();
-        
+        System.out.println("Enter The Elements size:");
+        int size = sc.nextInt();
+        for(int i=0;i<size;i++){
+            list.add(sc.nextInt());
+        }
+        sc.close();
+
     }
 }
