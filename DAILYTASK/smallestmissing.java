f151ecde2d9662a3acbdb9b2bc99e36708e19f28
poorna083 <dpoorna083@gmail.com>
2025-10-16 18:28:17 +0530

    smallestmissing

--- DAILYTASK/smallestmissing.java
+++ DAILYTASK/smallestmissing.java
@@ -1,4 +1,9 @@
 import java.util.Scanner;
+class Innersmallestmissing{
+    public int missingvalue(int [] array , int value){
+        
+    }
+}
 
 public class smallestmissing {
     public static void main(String[] args) {
@@ -6,5 +11,13 @@ public class smallestmissing {
         System.out.println("Enter The size:");
         int size = sc.nextInt();
         System.out.println("Enter The array:");
+        int [] array = new int[size];
+        for(int i=0;i<size;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.println("Enter The value:");
+        int value = sc.nextInt();
+        Innersmallestmissing i = new Innersmallestmissing();
+        System.out.println(i.missingvalue(array, value));
     }
 }
