45f1de697ed0fbc99dcc5c8785b281245ce76f9c
poorna083 <dpoorna083@gmail.com>
2025-10-16 18:24:19 +0530

    smallestmissing

---
+++ DAILYTASK/smallestmissing.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class smallestmissing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The size:");
+        int size = sc.nextInt();
+        System.out.println("Enter The array:");
+    }
+}
