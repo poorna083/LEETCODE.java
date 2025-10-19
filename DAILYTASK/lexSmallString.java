85381fb0738581c70e24d9c4a2126bdee378bd9a
poorna083 <dpoorna083@gmail.com>
2025-10-19 18:12:36 +0530

    lexSmallString

--- DAILYTASK/lexSmallString.java
+++ DAILYTASK/lexSmallString.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-
+class InnerlexSmallString{
+    
+}
 public class lexSmallString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
