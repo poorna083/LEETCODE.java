a613a5fd7c6c769ad663435fb0baec37ffb0abbb
poorna083 <dpoorna083@gmail.com>
2025-10-20 17:47:25 +0530

    valueVariable

--- DAILYTASK/valueVariable.java
+++ DAILYTASK/valueVariable.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+
 public class valueVariable {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String [] operations = new String[s.length()];
+        
+    }
 }
