a69a09779f02d13a456b0f04b85a2a2846f260ab
poorna083 <dpoorna083@gmail.com>
2025-10-21 18:01:24 +0530

    maxFrequency

--- DAILYTASK/maxFrequency.java
+++ DAILYTASK/maxFrequency.java
@@ -1,3 +1,15 @@
-public class maxFrequency {
+import java.util.Scanner;
+class InnermaxFrequency {
+
     
+}
+public class maxFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int [] nums = new int[size];
+        for(int i=0;i<size;i++) nums[i]=sc.nextInt();
+        int k = sc.nextInt();
+        int noOfoperations = sc.nextInt();
+    }
 }
