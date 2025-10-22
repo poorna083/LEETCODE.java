5d367ebed491cba79664ca8277a6c94ba450c214
poorna083 <dpoorna083@gmail.com>
2025-10-22 18:26:16 +0530

    addBinary

--- MATH/addBinary.java
+++ MATH/addBinary.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
 public class addBinary {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+    }
 }
