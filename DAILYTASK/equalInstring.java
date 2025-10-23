ad2e91973fce7b9d18611bf1d825203555f83408
poorna083 <dpoorna083@gmail.com>
2025-10-23 13:42:54 +0530

    equalInstring

--- DAILYTASK/equalInstring.java
+++ DAILYTASK/equalInstring.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-
+class InnerequalInstring {
+    public boolean hasSameDigits(String s) {
+        
+    }
+    
+}
 public class equalInstring {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
