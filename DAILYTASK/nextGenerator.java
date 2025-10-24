99279b3e995781cb9d719a412b2143fa08ec47d2
poorna083 <dpoorna083@gmail.com>
2025-10-24 18:12:28 +0530

    nextGenerator

--- DAILYTASK/nextGenerator.java
+++ DAILYTASK/nextGenerator.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
+class InnernextGenerator {
 
+    
+}
 public class nextGenerator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
