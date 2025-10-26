9168aa7409ef6d4786d315557a3e0bdedb486738
poorna083 <dpoorna083@gmail.com>
2025-10-26 18:27:57 +0530

    arrangeCoins

---
+++ DYNAMICPROGRAMMING/arrangeCoins.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class InnerarrangeCoins {
+    public int arrangeCoins(int n) {
+        int i = 1;
+		while(n > 0){ 
+			i++; 
+			n = n-i; 
+		}
+		return i-1; 
+    }
+    
+}
+public class arrangeCoins {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The N:");
+        int n = sc.nextInt();
+    }
+}
