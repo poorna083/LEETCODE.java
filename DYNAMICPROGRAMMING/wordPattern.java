b253e1e2cbcb5d618605ab0f34b270f9757b635a
poorna083 <dpoorna083@gmail.com>
2025-10-27 20:27:08 +0530

    wordPattern

--- DYNAMICPROGRAMMING/wordPattern.java
+++ DYNAMICPROGRAMMING/wordPattern.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class wordPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String pattern = sc.nextLine();
+        String s = sc.nextLine();
     }
 }
