446f680951043760dae90070f0bb2c3c27ccf3fc
poorna083 <dpoorna083@gmail.com>
2025-10-27 20:37:00 +0530

    wordPattern

--- DYNAMICPROGRAMMING/wordPattern.java
+++ DYNAMICPROGRAMMING/wordPattern.java
@@ -1,9 +1,28 @@
 import java.util.Scanner;
 class InnerwordPattern {
-    public boolean wordpattern(String pattern , String s){
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
         
+        if (pattern.length() != words.length) {
+            return false;}
+
+        HashMap<Character, String> charToWord = new HashMap<>();
+        HashSet<String> seenWords = new HashSet<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String w = words[i];
+
+            if (charToWord.containsKey(c)) {
+                if (!charToWord.get(c).equals(w)) {
+                    return false;}
+            } else {
+                if (seenWords.contains(w)) {
+                    return false;}
+                charToWord.put(c, w);
+                seenWords.add(w);}}
+        return true;
     }
-    
 }
 public class wordPattern {
     public static void main(String[] args) {
