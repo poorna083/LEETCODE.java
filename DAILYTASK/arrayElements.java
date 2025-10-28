11a522a086f5107e768435ce327db93bc3c656e1
poorna083 <dpoorna083@gmail.com>
2025-10-28 18:41:40 +0530

    arrayElements

--- DAILYTASK/arrayElements.java
+++ DAILYTASK/arrayElements.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
+class InnerarrayElements {
 
+    
+}
 public class arrayElements {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
