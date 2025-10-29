3a61072bdd33e7b38893442102791c478d0a158a
poorna083 <dpoorna083@gmail.com>
2025-10-29 18:30:22 +0530

    smallestNumber

--- DAILYTASK/smallestNumber.java
+++ DAILYTASK/smallestNumber.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 class InnersmallestNumber {
     public int smallestNumber(int n) {
         return -1 >>> Integer.numberOfLeadingZeros(n);
-    
+    }
 }
 public class smallestNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        InnersmallestNumber i = new InnersmallestNumber();
+        System.out.println(i.smallestNumber(n));
     }
 }
