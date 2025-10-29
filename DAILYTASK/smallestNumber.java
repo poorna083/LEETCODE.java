c659a405e8a91465b88a9ab39249e8a53dbd0e7a
poorna083 <dpoorna083@gmail.com>
2025-10-29 18:28:24 +0530

    smallestNumber

--- DAILYTASK/smallestNumber.java
+++ DAILYTASK/smallestNumber.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+
 public class smallestNumber {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
 }
