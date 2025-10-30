7d00a74b40d13f99d2c9f3996aaa425f0daa9f7c
poorna083 <dpoorna083@gmail.com>
2025-10-30 18:28:12 +0530

    ritchCustomer

--- ARRAYS/ritchCustomer.java
+++ ARRAYS/ritchCustomer.java
@@ -1,19 +1,19 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class ritchCustomer {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        int [] [] array = new int[size][size];
-        for(int i=0;i<array.length;i++){
-            for(int j=0;j<array[i].length;i++){
-                array[i][j]=sc.nextInt();
+        int[][] array = new int[size][size];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = sc.nextInt();
             }
         }
         for (int i = 0; i < array.length; i++) {
             System.out.println(Arrays.toString(array[i]));
         }
-        
     }
 }
+
