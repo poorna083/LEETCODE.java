b292fd8df249cd07506a6586522a4495d99492cd
poorna083 <dpoorna083@gmail.com>
2025-10-31 18:32:41 +0530

    sneekyNumbers

--- ARRAYS/sneekyNumbers.java
+++ ARRAYS/sneekyNumbers.java
@@ -2,6 +2,12 @@ import java.util.*;
 public class sneekyNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int  size = sc.nextInt();
+        int [] nums = new int[size];
+        for(int i=0;i<nums.length;i++){
+            nums[i]=sc.nextInt();
+        }
+
 
     }
 }
