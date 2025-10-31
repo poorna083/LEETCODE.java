b0ad541c19c17bee073480e01d6a0aadd009e407
poorna083 <dpoorna083@gmail.com>
2025-10-31 18:35:12 +0530

    sneekyNumbers

--- ARRAYS/sneekyNumbers.java
+++ ARRAYS/sneekyNumbers.java
@@ -1,4 +1,30 @@
 import java.util.*;
+class InnersneekyNumbers {
+    public int[] getSneakyNumbers(int[] nums){
+        public int[] getSneakyNumbers(int[] nums) {
+        int xor = 0;
+        int n = nums.length - 2;
+
+        for (int num : nums) xor ^= num;
+        for (int i = 0; i < n; i++) xor ^= i;
+
+        int diffBit = xor & -xor;
+
+        int a = 0, b = 0;
+        for (int num : nums) {
+            if ((num & diffBit) == 0) a ^= num;
+            else b ^= num;
+        }
+        for (int i = 0; i < n; i++) {
+            if ((i & diffBit) == 0) a ^= i;
+            else b ^= i;
+        }
+
+        return new int[]{a, b};
+    }
+    }
+    
+}
 public class sneekyNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
