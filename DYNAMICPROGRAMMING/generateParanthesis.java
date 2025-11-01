82d36f7a64bdb7dd3a897d0ddcd0ee88b521242c
poorna083 <dpoorna083@gmail.com>
2025-11-01 18:30:56 +0530

    generateParanthesis

---
+++ DYNAMICPROGRAMMING/generateParanthesis.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class generateParanthesis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
