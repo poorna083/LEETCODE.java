4ba20c41ac330b95d30091e1757f42b2b8544bf3
poorna083 <dpoorna083@gmail.com>
2025-11-02 18:27:40 +0530

    reShape

--- MATRIX/reShape.java
+++ MATRIX/reShape.java
@@ -1,9 +1,20 @@
 package MATRIX;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class reShape {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int [] []mat = new int[size][size];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                mat[i][j]=sc.nextInt();
+            }
+        }
+        for (int i = 0; i < mat.length; i++) {
+            System.out.println(Arrays.toString(mat));
+        }
     }
 }
