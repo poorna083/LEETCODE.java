311c4f2b996fde061f655e16e26934ebf978de50
poorna083 <dpoorna083@gmail.com>
2025-11-02 18:32:07 +0530

    reShape

--- MATRIX/reShape.java
+++ MATRIX/reShape.java
@@ -3,8 +3,7 @@ package MATRIX;
 import java.util.Arrays;
 import java.util.Scanner;
 class InnerreShape {
-    public int[][] matrixReshape(int[][] mat, int r, int c) {
-        public int[][] matrixReshape(int[][] nums, int r, int c) {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
     int n = nums.length, m = nums[0].length;
     if (r*c != n*m) return nums;
     int[][] res = new int[r][c];
@@ -14,20 +13,19 @@ class InnerreShape {
 }
     }
     
-}
 public class reShape {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        int [] []mat = new int[size][size];
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[i].length; j++) {
-                mat[i][j]=sc.nextInt();
+        int [] []nums = new int[size][size];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                nums[i][j]=sc.nextInt();
             }
         }
         int r = sc.nextInt();
         int c = sc.nextInt();
         InnerreShape i = new InnerreShape();
-        System.out.println(i.matrixReshape(mat, r, c));
+        System.out.println(i.matrixReshape(nums, r, c));
     }
 }
