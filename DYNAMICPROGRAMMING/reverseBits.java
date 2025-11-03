9f261239069d0c162afd8431ec3977705c5a8200
poorna083 <dpoorna083@gmail.com>
2025-11-03 17:52:32 +0530

    reverseBits

--- DYNAMICPROGRAMMING/reverseBits.java
+++ DYNAMICPROGRAMMING/reverseBits.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
-
+class InnerreverseBits {
+    public int reverseBits(int n) {
+         int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>= 1;
+        }
+        return ans;
+    }
+};
 public class reverseBits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-    }
+}
 }
