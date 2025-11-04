c5f0d2fc64dd54f3c2d86fd65e334308dca591df
poorna083 <dpoorna083@gmail.com>
2025-11-04 18:21:06 +0530

    klongArrays

---
+++ DAILYTASK/klongArrays.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class klongArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int [] nums = new int[size];
+        for(int i=0;i<nums.length;i++){
+            nums[i]=sc.nextInt();
+        }
+        int k = sc.nextInt();
+        int x = sc.nextInt();
+    }
+}
