32178315e699fe9290697ee7c2ebdf3c4c04fc61
poorna083 <dpoorna083@gmail.com>
2025-11-05 18:31:09 +0530

    relativeRanks

---
+++ ARRAYS/relativeRanks.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class relativeRanks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int [] nums = new int[size];
+        for(int i=0;i<size;i++){
+            nums[i]=sc.nextInt();
+        }
+    }
+}
