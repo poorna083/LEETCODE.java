f4ce743b020add4185cd5e1e2a724bbf0d71fc7b
poorna083 <dpoorna083@gmail.com>
2025-11-06 19:13:02 +0530

    arrayPartation

--- SORTING/arrayPartation.java
+++ SORTING/arrayPartation.java
@@ -1,5 +1,17 @@
 import java.util.Scanner;
-
+class InnerarrayPartation {
+    public int arrayPairSum(int[] nums) {
+            
+        int sum = 0;
+        Arrays.sort(nums);
+            
+        for(int i=0; i<nums.length; i=i+2) 
+            sum += nums[i];
+        
+        return sum;
+    }
+    
+}
 public class arrayPartation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
