8bd16d3125cfdcda338499e6f7f6df1172954947
poorna083 <dpoorna083@gmail.com>
2025-11-06 19:13:33 +0530

    arrayPartation

--- SORTING/arrayPartation.java
+++ SORTING/arrayPartation.java
@@ -20,6 +20,7 @@ public class arrayPartation {
         for(int i=0;i<size;i++){
             nums[i]=sc.nextInt();
         }
-
+        InnerarrayPartation i = new InnerarrayPartation();
+        System.out.println(i.arrayPairSum(nums));
     }
 }
