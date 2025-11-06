76c48df689d60b2ea527f2c82bdb54210384f73c
poorna083 <dpoorna083@gmail.com>
2025-11-06 19:12:25 +0530

    arrayPartation

--- SORTING/arrayPartation.java
+++ SORTING/arrayPartation.java
@@ -3,5 +3,11 @@ import java.util.Scanner;
 public class arrayPartation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int [] nums = new int[size];
+        for(int i=0;i<size;i++){
+            nums[i]=sc.nextInt();
+        }
+
     }
 }
