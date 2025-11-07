fc559de2a4dc7fe3e43a5b4973706cd505aa08b1
poorna083 <dpoorna083@gmail.com>
2025-11-07 19:00:33 +0530

    candySwap

--- SORTING/candySwap.java
+++ SORTING/candySwap.java
@@ -5,6 +5,10 @@ public class candySwap {
         Scanner sc = new Scanner(System.in);
         int bobsize = sc.nextInt();
         int alicesize = sc.nextInt();
-        
+        int [] bob = new int[bobsize];
+        int [] alice = new int[alicesize];
+        for(int i=0;i<bobsize;i++) bob[i]=sc.nextInt();
+        for(int i=0;i<alicesize;i++)alice[i]=sc.nextInt();
+
     }
 }
