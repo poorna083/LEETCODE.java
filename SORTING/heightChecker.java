3b200f5efa1f91b06a9bb2966828d1d91991d007
poorna083 <dpoorna083@gmail.com>
2025-11-08 19:00:51 +0530

    heightChecker

--- SORTING/heightChecker.java
+++ SORTING/heightChecker.java
@@ -5,5 +5,8 @@ public class heightChecker {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int [] array = new int[size];
+        for(int i=0;i<size;i++){
+            array[i]=sc
+        }
     }
 }
