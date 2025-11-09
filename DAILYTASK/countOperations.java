07e07c8d43779b36513eb63c5ed7b3c7715943b6
poorna083 <dpoorna083@gmail.com>
2025-11-09 18:25:52 +0530

    countOperations

---
+++ DAILYTASK/countOperations.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class InnercountOperations {
+    public int CountTheOperations(int num1,int num2){
+
+    }
+    
+}
+public class countOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        InnercountOperations i = new InnercountOperations();
+        System.out.println(i.CountTheOperations(num1, num2));
+    }
+}
