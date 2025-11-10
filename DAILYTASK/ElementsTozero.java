10b2360a5a26f52c0e53fe9ba852b5cf1a23fae0
poorna083 <dpoorna083@gmail.com>
2025-11-10 14:53:10 +0530

    ElementsTozero

--- DAILYTASK/ElementsTozero.java
+++ DAILYTASK/ElementsTozero.java
@@ -4,6 +4,6 @@ public class ElementsTozero {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        int [] nums = new int[size];
+        int [] nums = new int[size]; for(int i=0;i<size;i++) nums[i]=sc.nextInt();
     }
 }
