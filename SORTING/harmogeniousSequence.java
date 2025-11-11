b899e7ea810c15f721594496d4157d0f0cafcc79
poorna083 <dpoorna083@gmail.com>
2025-11-11 18:12:31 +0530

    harmogeniousSequence

--- SORTING/harmogeniousSequence.java
+++ SORTING/harmogeniousSequence.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
-
+class InnerharmogeniousSequence {
+    public int findLHS(int [] nums){
+        
+    }
+    
+}
 public class harmogeniousSequence {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int [] nums = new int[size];
         for(int i=0;i<size;i++)nums[i]=sc.nextInt();
+        sc.close();
     }
 }
