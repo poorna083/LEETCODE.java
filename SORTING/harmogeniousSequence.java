47751c5aa14b10adee18f7c71ae2391f46107247
poorna083 <dpoorna083@gmail.com>
2025-11-11 18:16:14 +0530

    harmogeniousSequence

--- SORTING/harmogeniousSequence.java
+++ SORTING/harmogeniousSequence.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
+import java.util.Arrays;
 class InnerharmogeniousSequence {
     public int findLHS(int [] nums){
-        
+        Arrays.sort(nums);
+        int l = 0, r = 1, res = 0;
+        while(r < nums.length) {
+            int diff = nums[r] - nums[l];
+            if(diff == 1) res = Math.max(res, r - l + 1);
+            if(diff <= 1) r++;
+            else l++;
+        }
+
+        return res;
     }
     
 }
