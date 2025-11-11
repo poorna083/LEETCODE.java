2794e324836a11b34e62a4fd47b364ac935e3210
poorna083 <dpoorna083@gmail.com>
2025-11-11 18:11:38 +0530

    harmogeniousSequence

--- SORTING/harmogeniousSequence.java
+++ SORTING/harmogeniousSequence.java
@@ -5,5 +5,6 @@ public class harmogeniousSequence {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int [] nums = new int[size];
+        for(int i=0;i<size;i++)nums[i]=sc.nextInt();
     }
 }
