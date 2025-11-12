ab9c2526e2df74a5fa412f88679a013d0f4ba416
poorna083 <dpoorna083@gmail.com>
2025-11-12 18:55:14 +0530

    operationsEqulasto1

---
+++ DAILYTASK/operationsEqulasto1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class operationsEqulasto1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int nums [] = new int[size];
+    }
+}
