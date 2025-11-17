eac50b1c0310c1492f697e1466219c7516ae6086
poorna083 <dpoorna083@gmail.com>
2025-11-17 18:18:44 +0530

    onesLength

---
+++ DAILYTASK/onesLength.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class onesLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int [] nums = new int[size];
+        for(int i=0;i<size;i++)nums[i]=sc.nextInt();
+        int k = sc.nextInt();
+    }
+}
