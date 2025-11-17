713ec806745cf68e27ef82c09889092cbc173b30
poorna083 <dpoorna083@gmail.com>
2025-11-17 18:19:48 +0530

    onesLength

--- DAILYTASK/onesLength.java
+++ DAILYTASK/onesLength.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
+class InneronesLength {
+    public boolean kLengthApart(int[] nums, int k) {
+        int lastIndex = -1;
 
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 1) {
+                if (lastIndex != -1 && i - lastIndex - 1 < k) {
+                    return false;
+                }
+                lastIndex = i;
+            }
+        }
+
+        return true;
+    }
+    
+}
 public class onesLength {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -7,5 +24,6 @@ public class onesLength {
         int [] nums = new int[size];
         for(int i=0;i<size;i++)nums[i]=sc.nextInt();
         int k = sc.nextInt();
+
     }
 }
