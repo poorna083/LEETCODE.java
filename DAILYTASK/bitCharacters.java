7ccbd769c8aa454d6d10bc249f39f7ca72fc25ba
poorna083 <dpoorna083@gmail.com>
2025-11-18 21:46:11 +0530

    bitCharacters

--- DAILYTASK/bitCharacters.java
+++ DAILYTASK/bitCharacters.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class bitCharacters {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int [] bits = new int[size];
     }
 }
