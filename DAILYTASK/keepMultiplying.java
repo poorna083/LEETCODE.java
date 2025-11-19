378419921844c28873e7b25bc3a49ca711dee35f
poorna083 <dpoorna083@gmail.com>
2025-11-19 18:54:20 +0530

    keepMultiplying

--- DAILYTASK/keepMultiplying.java
+++ DAILYTASK/keepMultiplying.java
@@ -4,6 +4,8 @@ public class keepMultiplying {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        int [] nums 
+        int [] nums = new int[size];
+        for(int i=0;i<size;i++)
+        nums[i]=sc.nextInt();
     }
 }
