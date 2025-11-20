81fc002ac0c51404762fadc1d4c610baaaaa3715
poorna083 <dpoorna083@gmail.com>
2025-11-20 18:55:30 +0530

    teemoAttacking

--- ARRAYS/teemoAttacking.java
+++ ARRAYS/teemoAttacking.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
+class InnerteemoAttacking {
 
+    
+}
 public class teemoAttacking {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
