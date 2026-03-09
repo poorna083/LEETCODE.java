7d0cb0c0d61c98862cf37b284edc94a5bb6ee2e4
poorna083 <dpoorna083@gmail.com>
2026-03-09 19:51:51 +0530

    ReverseString

--- ARRAYS/reverseString.java
+++ ARRAYS/reverseString.java
@@ -1,20 +1,11 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class reverseString {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+class InnReverseHelper {
 
-        System.out.println("Enter the size:");
-        int size = sc.nextInt();
-
-        char[] array = new char[size];
-
-        for(int i = 0; i < size; i++){
-            array[i] = sc.next().charAt(0);
-        }
+    static void reverse(char[] array){
         int start = 0;
-        int end = size - 1;
+        int end = array.length - 1;
 
         while(start < end){
             char temp = array[start];
@@ -26,6 +17,25 @@ public class reverseString {
         }
 
         System.out.println(Arrays.toString(array));
+    }
+}
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size:");
+        int size = sc.nextInt();
+
+        char[] array = new char[size];
+
+        for(int i = 0; i < size; i++){
+            array[i] = sc.next().charAt(0);
+        }
+
+        InnReverseHelper.reverse(array);
+
         sc.close();
     }
 }
